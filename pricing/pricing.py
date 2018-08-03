6a7506d9e9647fc6382ba6f7712555dc78757bb6
ep <epedrotti@indeep.ai>
2018-08-03 20:34:55 +0200

    pricing service

---
+++ pricing/pricing.py
@@ -0,0 +1,49 @@
+import os
+import time
+import requests
+import json
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print("connected with rc", rc)
+
+def on_message(client, userdata, msg):
+    message = "%s %s" % msg.topic, str(msg.payload)
+
+def get_prices():
+    url = 'https://api.coinmarketcap.com/v2/ticker/'
+    resp = requests.get(url)
+    data = resp.json()['data']
+    listing = create_listing(data)
+    return listing
+
+def create_listing(data):
+    toReturn = dict()
+    for key, value in data.items():
+        key = value['symbol']
+        toReturn[key] = value['quotes']['USD']['price']
+    return toReturn
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+host = os.getenv('HOST')
+port = int(os.getenv('PORT')) 
+client.connect(host, port, 60)
+
+oldPrices = dict()
+
+while True:
+    print("updating prices")
+    prices = get_prices()
+    for key, value in prices.items():
+        if not key in oldPrices.keys():
+            oldPrices[key] = value
+        elif oldPrices[key] == value:
+            continue
+        topic = "pricing/%s" % key
+        client.publish(topic, value)
+    time.sleep(5)
+client.loop_forever()
+
